9012fb4d6f8cc35327e7ba4c93dcac58a6f40401
Jeffrey Brown <brownjs03@gmail.com>
2019-12-04 01:47:19 +0000

    Adding API fetch file

--- lib/nfl_cli/api.rb
+++ lib/nfl_cli/api.rb
@@ -1,1 +1,11 @@
 NflCli::API
+require 'httparty'
+require 'pry'
+
+class API 
+  
+  def fetch 
+  url = "http://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/"
+  response = HTTParty.get(url)
+  binding.pry
+end
