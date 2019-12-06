c8bcda827298da0b3d218df19632b7bd32173e56
Jeffrey Brown <brownjs03@gmail.com>
2019-12-06 05:44:00 +0000

    Updated fetch method in Api Class

--- lib/nfl_cli/api.rb
+++ lib/nfl_cli/api.rb
@@ -3,10 +3,14 @@ require 'pry'
 
 class Api 
   
-  def fetch 
-  url = "http://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/"
+  def self.fetch  
+  url = "http://site.api.espn.com/apis/site/v2/sports/football/nfl/teams"
   response = HTTParty.get(url)
+  response["sports"][0]["leagues"][0]["teams"].each do |team|
+    name = team["team"]["displayName"]
+    record = team["team"]["record"]["items"][0]["summary"]
   binding.pry
+  end 
   end
-
+  
 end 
