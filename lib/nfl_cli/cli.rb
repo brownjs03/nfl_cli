713adcc9e747d063b211f161aac57198b4cb0cca
Jeffrey Brown <brownjs03@gmail.com>
2019-12-07 19:53:20 +0000

    begin menu method

--- lib/nfl_cli/cli.rb
+++ lib/nfl_cli/cli.rb
@@ -2,14 +2,20 @@ class Cli
   
   def start 
     Api.fetch
+    puts "NFL season is here! Which team would you like to know more about?"
     list_teams
-    
-  end 
+    menu 
+    end 
   
   def list_teams
     Team.all.each_with_index do |team, index|
       puts "#{index + 1}. #{team.name}"
     end 
+  end 
+  
+  def menu
+    puts "Please enter the number for the team you would like to see more about!"
+    
   end 
   
 end 
