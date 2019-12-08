759064e9214047d68d116c895430f101e89e96a0
Jeffrey Brown <brownjs03@gmail.com>
2019-12-08 19:53:16 +0000

    Menu loops through team selections. Allows list, exit

--- lib/nfl_cli/cli.rb
+++ lib/nfl_cli/cli.rb
@@ -14,14 +14,27 @@ class Cli
   end 
   
   def menu
-    puts "Please enter the number for the team you would like to see more about!" 
-    input = gets.strip 
-    if input.to_i > 0 
-      puts Team.all[input.to_i - 1].name
-      puts "Record: #{Team.all[input.to_i - 1].record}"
-      puts "Schedule: #{Team.all[input.to_i - 1].schedule_link}"
-      puts "Roster: #{Team.all[input.to_i - 1].roster_link}"
-      puts "Injury Report: #{Team.all[input.to_i - 1].injury_report}"
+    puts "Please enter the number for the team you would like to see more about. At any time type 'list' to see the teams again or 'exit' to exit the program." 
+    input = gets.strip.downcase 
+    if input.to_i > 0 && input.to_i <= Team.all.count
+      team = Team.all[input.to_i - 1]
+      puts 
+      puts team.name
+      puts "Record: #{team.record}"
+      puts "Schedule: #{team.schedule_link}"
+      puts "Roster: #{team.roster_link}"
+      puts "Injury Report: #{team.injury_report}"
+      puts 
+      menu
+      elsif input == "list"
+      list_teams
+      menu 
+    elsif input == "exit"
+    puts "See you next week!"
+    exit 
+    else puts 
+      puts "I'm sorry, That input was not recognized."
+      menu
     end 
   end 
   
