6cdc918d7610a987590fdd7f17659a39e9cbcec7
Jeffrey Brown <brownjs03@gmail.com>
2019-12-20 01:00:05 +0000

    refactored team_display method

--- lib/nfl_cli/cli.rb
+++ lib/nfl_cli/cli.rb
@@ -5,7 +5,7 @@ class Cli
     puts "NFL season is here! Which team would you like to know more about?"
     list_teams
     menu 
-    end 
+  end 
   
   def list_teams
     Team.all.each.with_index(1) do |team, i|
@@ -16,10 +16,26 @@ class Cli
   def menu
     input = nil 
     while input != "exit"
-    puts "Please enter the number for the team you would like to see more about. At any time type 'list' to see the teams again or 'exit' to exit the program." 
-    input = gets.strip.downcase 
-    if input.to_i > 0 && input.to_i <= Team.all.count
-      team = Team.all[input.to_i - 1]
+      puts "Please enter the number for the team you would like to see more about. At any time type 'list' to see the teams again or 'exit' to exit the program." 
+      input = gets.strip.downcase 
+      if input.to_i > 0 && input.to_i <= Team.all.count
+        self.display_team(input)
+      elsif input == "list"
+        list_teams
+      elsif input == "exit"
+        quit 
+      else 
+        puts "I'm sorry, That input was not recognized."
+      end 
+    end 
+  end 
+  
+  def quit 
+    puts "See you next week for your NFL needs!"
+  end 
+  
+  def display_team(input)
+    team = Team.all[input.to_i - 1]
       puts 
       puts team.name
       puts "Record: #{team.record}"
@@ -27,18 +43,6 @@ class Cli
       puts "Roster: #{team.roster_link}"
       puts "Injury Report: #{team.injury_report}"
       puts 
-      elsif input == "list"
-      list_teams
-    elsif input == "exit"
-    quit 
-    else puts 
-      puts "I'm sorry, That input was not recognized."
-    end 
-  end 
-  end 
-  
-  def quit 
-    puts "See you next week for your NFL needs!"
-  end 
+  end
   
 end 
